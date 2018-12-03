1e5e6b368b4678ec8d1ca362998f6524176bb6d2
Thomas Maragno <thomas.maragno@oracle.com>
2018-12-03 16:10:28 -0400

    change tests

--- src/test/java/io/helidon/examples/quickstart/se/MainTest.java
+++ src/test/java/io/helidon/examples/quickstart/se/MainTest.java
@@ -67,7 +67,7 @@ public class MainTest {
         Assertions.assertEquals(200, conn.getResponseCode(), "HTTP response2");
         jsonReader = Json.createReader(conn.getInputStream());
         jsonObject = jsonReader.readObject();
-        Assertions.assertEquals("Hello Costa Rica!", jsonObject.getString("message"),
+        Assertions.assertEquals("Hello Joe!", jsonObject.getString("message"),
                 "hello Joe message");
 
         conn = getURLConnection("PUT", "/greet/greeting/Hola");
@@ -76,7 +76,7 @@ public class MainTest {
         Assertions.assertEquals(200, conn.getResponseCode(), "HTTP response4");
         jsonReader = Json.createReader(conn.getInputStream());
         jsonObject = jsonReader.readObject();
-        Assertions.assertEquals("Hola World!", jsonObject.getString("message"),
+        Assertions.assertEquals("Hola Jose!", jsonObject.getString("message"),
                 "hola Jose message");
     }
 
